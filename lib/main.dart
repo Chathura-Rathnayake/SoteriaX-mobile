8131736119ab309fe372c1f8d231dbe3b744be7b
Harshana Hasanka <hhasankae@gmail.com>
2021-07-19 16:02:43 +0530

    authentication services file

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,76 @@
-import 'package:flutter/cupertino.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:soteriax/screens/error_page.dart';
-import 'package:soteriax/screens/forgot_password.dart';
-import 'package:soteriax/screens/login.dart';
-
+import 'package:provider/provider.dart';
+import 'package:soteriax/models/lifeguard.dart';
+import 'package:soteriax/screens/initialization/error_page.dart';
+import 'package:soteriax/screens/initialization/init_loading.dart';
+import 'package:soteriax/screens/wrapper.dart';
+import 'package:soteriax/services/auth_services.dart';
 void main() {
-  runApp(
-      MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Login(),
-      ),
-  );
+  runApp(MyApp());
 }
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  //default init and error state to false
+  bool _initialized=false;
+  bool _error=false;
+
+  void initializeFlutterFire() async{
+    try{
+      //await for firebase to be initalized and set init state to true when it does
+      await Firebase.initializeApp();
+      setState(() {
+        _initialized=true;
+      });
+    }catch(e){
+      setState(() {
+        _error=true;
+        print(e.toString());
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    initializeFlutterFire();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if(_error){
+      return Error_Page();
+    }
+    if(!_initialized){
+      return MaterialApp(home: LoadingSpinner());
+    }
+
+    return MultiProvider(
+      providers: [
+          ChangeNotifierProvider<AuthService>.value(value: AuthService()),
+          StreamProvider<Sys_user?>.value(
+          value: AuthService().authStateChanges,
+          initialData: null,
+          ),
+      ],
+          child: MaterialApp(
+              home: Wrapper()
+          )
+      );
+
+  }
+}
+
+
+
 
 
 
---
+++ lib/services/auth_services.dart
@@ -0,0 +1,67 @@
+import 'dart:io';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:soteriax/models/lifeguard.dart';
+
+class AuthService with ChangeNotifier{
+
+  bool _isLoading=false;
+  String _errorMessage="";
+  bool get isLoading => _isLoading;
+  String get errorMessage => _errorMessage;
+
+  void setLoading(val){
+    _isLoading=val;
+    notifyListeners();
+  }
+
+  void setMessage(message){
+    _errorMessage=message;
+    notifyListeners();
+  }
+
+  final FirebaseAuth _auth=FirebaseAuth.instance;
+
+  Sys_user? _Sys_UserFromFirebaseUser(User? user){
+    if(user==null){
+      return null;
+    }else{
+      return Sys_user(uid: user.uid, email: user.email);
+    }
+  }
+
+  Future signInWithEmailAndPassword(String email, String password) async{
+    setLoading(true);
+    try{
+      UserCredential result=await _auth.signInWithEmailAndPassword(email: email, password: password);
+      setLoading(false);
+      return _Sys_UserFromFirebaseUser(result.user);
+    }on SocketException{
+      setLoading(false);
+      setMessage("No internet, please check your internet connection");
+    }
+    on FirebaseAuthException catch(e){
+      setLoading(false);
+      setMessage(e.message);
+    }
+    notifyListeners();
+
+  }
+
+  Future<void> signOut() async{
+    try{
+      return await _auth.signOut();
+    }catch(e){
+      print(e.toString());
+      return null;
+    }
+  }
+
+
+
+  //stream for user auth state changes
+  Stream<Sys_user?> get authStateChanges => _auth.idTokenChanges().map((user) => _Sys_UserFromFirebaseUser(user));
+
+
+}
