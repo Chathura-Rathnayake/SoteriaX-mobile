7dc2bc234d056e80933e39afd19752ed53287a88
Harshana Hasanka <hhasankae@gmail.com>
2021-07-19 16:03:16 +0530

    user model class for lifeguards

---
+++ lib/models/lifeguard.dart
@@ -0,0 +1,20 @@
+class Sys_user{
+  Sys_user({this.uid, this.email});
+  final String? uid;
+  final String? email;
+}
+
+
+class Lifeguard{
+
+  Lifeguard({this.username, this.email, this.firstname, this.lastname, this.age, this.number_of_ops});
+
+  String? username;
+  String? firstname;
+  String? lastname;
+  String? email;
+  int? age;
+  int? number_of_ops;
+
+
+}
