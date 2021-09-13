5194d71441b2985e8b441cda6087bac103205671
nukezzzz <lawthenuka@gmail.com>
2021-09-13 09:21:35 +0530

    droping function

---
+++ lib/services/raspberryPi/drop_package.dart
@@ -0,0 +1,16 @@
+/*
+// import 'package:ssh2/ssh2.dart';
+//
+// class DropPackageRPI {
+//   connectToRPI() async {
+//     var client = new SSHClient(
+//       host: "192.168.91.136",
+//       port: 22,
+//       username: "k8user",
+//       passwordOrKey: "1234Qwer",
+//     );
+//     await client.connect();
+//     var result = await client.execute("mkdir new");
+//   }
+// }
+*/
