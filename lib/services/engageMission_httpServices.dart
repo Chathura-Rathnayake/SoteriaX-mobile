32d456793d9af78dc921bc25272fb154e4b6d785
nukezzzz <lawthenuka@gmail.com>
2021-09-16 11:34:21 +0530

    test01

--- lib/services/engageMission_httpServices.dart
+++ lib/services/engageMission_httpServices.dart
@@ -2,32 +2,28 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:http/http.dart' as http;
-class EngageMissionHttp{
+
+class EngageMissionHttp {
   var url;
 
-  EngageMissionHttp(){
-    url=Uri.parse('http://192.168.43.5:3000');
+  EngageMissionHttp() {
+    url = Uri.parse('http://192.168.43.5:3000');
   }
 
-  Future<Map?> checkRPiAvailability() async{
-    Map map={'status':null};
-    var client=http.Client();
-    try{
-      http.Response uriResponse=await client.post(url, body: {'name': 'doodle', 'color': 'blue'});
-      map['status']=jsonDecode(uriResponse.statusCode.toString());
+  Future<Map?> checkRPiAvailability() async {
+    Map map = {'status': null};
+    var client = http.Client();
+    try {
+      http.Response uriResponse =
+          await client.post(url, body: {'name': 'doodle', 'color': 'blue'});
+      map['status'] = jsonDecode(uriResponse.statusCode.toString());
       // print('Response body: ${uriResponse.body}');
-    }on SocketException catch (e){
-      map['status']=e.osError!.errorCode;
+    } on SocketException catch (e) {
+      map['status'] = e.osError!.errorCode;
       print(e.osError!.errorCode);
-    }finally{
+    } finally {
       client.close();
     }
     return map;
   }
-  
-
-
-
-
-
 }
