19006abf5f89fe5f5a2a4123627302383dfe6cc7
husseyhh <56592096+husseyhh@users.noreply.github.com>
2021-09-19 14:05:46 +0530

    webrtc with recordung support

--- lib/screens/home/training_operation.dart
+++ lib/screens/home/training_operation.dart
@@ -27,7 +27,7 @@ class TrainingOperation extends StatefulWidget {
 class _TrainingOperationState extends State<TrainingOperation> {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   int? type;
-  WebRTCServices webRTCServices = WebRTCServices();
+  late WebRTCServices webRTCServices;
   RTCVideoRenderer remoteRenderer = RTCVideoRenderer();
   Timer? stopWatchTimeStampTimer;
   bool isWaitingStopWatchPing = false;
@@ -78,6 +78,7 @@ class _TrainingOperationState extends State<TrainingOperation> {
   @override
   void initState() {
     trainingOpDB=TrainingOperationsDBServices(operationId: widget.trainingOpId);
+    webRTCServices = WebRTCServices(operationId: widget.trainingOpId, operationType: 'training');
     print('training op id: ${widget.trainingOpId}');
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.landscapeRight,
@@ -97,7 +98,6 @@ class _TrainingOperationState extends State<TrainingOperation> {
 
   @override
   void deactivate() {
-    // TODO: implement deactivate
     super.deactivate();
     stopWatchTimeStampTimer?.cancel();
   }
@@ -218,7 +218,7 @@ class _TrainingOperationState extends State<TrainingOperation> {
                           if(snapshot.data!=null){
                             var operationStatus=snapshot.data!.get("operationStatus");
                             var currentStage=snapshot.data!.get("currentStage");
-                            if(operationStatus=='pending'){
+                            if(operationStatus=='pending'){ //hasn't been initiated  yet
                               return Column(
                                 children: [
                                   Container(
@@ -243,8 +243,8 @@ class _TrainingOperationState extends State<TrainingOperation> {
                                   Container(child: Text("Please start the training operation"),),
                                 ],
                               );
-                            }else if(operationStatus=='live'){
-                              if(currentStage<5){
+                            }else if(operationStatus=='live'){ //live ongoing
+                              if(currentStage<5){  //ongoing
                                 return Column(
                                   children: [
                                     Container(
@@ -309,7 +309,10 @@ class _TrainingOperationState extends State<TrainingOperation> {
                                     ),
                                   ],
                                 );
-                              }else{
+                              }else{ //being uploaded
+                                if(_stopWatchTimer.isRunning){
+                                  _stopWatchTimer.onExecute.add(StopWatchExecute.stop);
+                                }
                                 return Column(
                                   children: [
                                     Container(
@@ -336,7 +339,9 @@ class _TrainingOperationState extends State<TrainingOperation> {
                                 );
                               }
                             }else{
-                              _stopWatchTimer.onExecute.add(StopWatchExecute.stop);
+                              if(_stopWatchTimer.isRunning){
+                                _stopWatchTimer.onExecute.add(StopWatchExecute.stop);
+                              }
                               return Column(
                                 children: [
                                   Container(
@@ -363,11 +368,20 @@ class _TrainingOperationState extends State<TrainingOperation> {
                               );
                             }
                           }else{
+                            if(_stopWatchTimer.isRunning){
+                              _stopWatchTimer.onExecute.add(StopWatchExecute.stop);
+                            }
                             return Container(child: Text("Training operation has been removed"),);
                           }
                         }else if(snapshot.hasError){
+                          if(_stopWatchTimer.isRunning){
+                            _stopWatchTimer.onExecute.add(StopWatchExecute.stop);
+                          }
                           return Container(child: Text("Error occurred while retrieving data"),);
                         }else{
+                          if(_stopWatchTimer.isRunning){
+                            _stopWatchTimer.onExecute.add(StopWatchExecute.stop);
+                          }
                           return Container(child: Text("No training data"),);
                         }
                       }
