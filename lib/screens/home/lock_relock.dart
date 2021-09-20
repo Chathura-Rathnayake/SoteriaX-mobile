fbad0bb9a4c35fd6b6481a0968084ff6fbec22e0
husseyhh <56592096+husseyhh@users.noreply.github.com>
2021-09-20 23:08:54 +0530

    lock relock considering training operations

--- lib/screens/home/lock_relock.dart
+++ lib/screens/home/lock_relock.dart
@@ -4,6 +4,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:soteriax/database/operations_database_services.dart';
+import 'package:soteriax/database/training_operations_database_services.dart';
 import 'package:soteriax/models/lifeguard.dart';
 import 'package:soteriax/screens/initialization/init_loading.dart';
 
@@ -97,103 +98,164 @@ class _LockState extends State<Lock> {
                         ],
                       )),
                 ),
-                StreamBuilder<QuerySnapshot?>(
-                  stream: OperationDatabaseService().getLiveOperationData,
-                  builder: (context, snapshot) {
-                    if(snapshot.hasError){
-                      return Container(child: Text("Error occurred while connecting to Database"),);
-                    }else if(snapshot.hasData){
-                      if(snapshot.data!.size==0){
-                        return Expanded(
-                          child: GridView.count(
-                            primary: false,
-                            padding: const EdgeInsets.only(
-                                top: 30, left: 80, right: 80, bottom: 10),
-                            crossAxisSpacing: 0,
-                            mainAxisSpacing: 20,
-                            crossAxisCount: 1,
-                            children: <Widget>[
-                              Container(
-                                padding: const EdgeInsets.only(
-                                    top: 0, left: 10, right: 10, bottom: 0),
-                                child: (MaterialButton(
-                                  onPressed: () {},
-                                  height: 20,
-                                  minWidth: 20,
-                                  elevation: 10,
-                                  color: Colors.white,
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10),
-                                  ),
-                                  child: Container(
-                                    child: Column(
-                                        mainAxisAlignment: MainAxisAlignment.center,
-                                        children: <Widget>[
-                                          Image(
-                                            image: AssetImage(
-                                              'assets/icons/lock.png',
-                                            ),
-                                            height: 75,
-                                          ),
-                                          SizedBox(height: 20,),
-                                          Text(
-                                            'Engage Mission',
-                                            textAlign: TextAlign.center,
-                                            style: TextStyle(
-                                              fontSize: 15,
-                                              fontWeight: FontWeight.w700,
-                                            ),
-                                          ),
-                                        ]),
-                                  ),
-                                )),
-                              ),
-                              Container(
-                                padding: const EdgeInsets.only(
-                                    top: 0, left: 10, right: 10, bottom: 0),
-                                child: (MaterialButton(
-                                  onPressed: () {},
-                                  height: 10,
-                                  minWidth: 20,
-                                  elevation: 10,
-                                  color: Colors.white,
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10),
-                                  ),
-                                  child: Container(
-                                    child: Column(
-                                        mainAxisAlignment: MainAxisAlignment.center,
-                                        children: <Widget>[
-                                          Image(
-                                            image: AssetImage(
-                                              'assets/icons/unlock.png',
-                                            ),
-                                            height: 75,
-                                            alignment: Alignment.center,
-                                          ),
-                                          SizedBox(height: 20,),
-                                          Text(
-                                            'Engage Mission',
-                                            textAlign: TextAlign.center,
-                                            style: TextStyle(
-                                              fontSize: 15,
-                                              fontWeight: FontWeight.w700,
-                                            ),
-                                          ),
-                                        ]),
-                                  ),
-                                )),
-                              ),
+                Column(
+                  children: [
+                    StreamBuilder<QuerySnapshot?>(
+                      stream: OperationDatabaseService().getLiveOperationData,
+                      builder: (context, snapshot) {
+                        if(snapshot.hasError){
+                          return Column(
+                            children: [
+                              SizedBox(height: 70,),
+                              Icon(Icons.not_interested_outlined, color: Colors.red, size: 50,),
+                              SizedBox(height: 20,),
+                              Container(child: Text("Error occurred while connecting to Database", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),),),
                             ],
-                          ),
-                        );
-                      }else{
-                        return Container(child: Text("Ongoing live operation", ),);
+                          );
+                        }else if(snapshot.hasData){
+                          if(snapshot.data!.size==0){
+                            return StreamBuilder<QuerySnapshot?>(
+                              stream: TrainingOperationsDBServices().getLiveTrainingOperationData,
+                              builder: (context, snapshot) {
+                               if(snapshot.hasError){
+                                 return Column(
+                                   children: [
+                                     SizedBox(height: 70,),
+                                     Icon(Icons.not_interested_outlined, color: Colors.red, size: 50,),
+                                     SizedBox(height: 20,),
+                                     Container(child: Text("Error occurred while connecting to Database", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),),),
+                                   ],
+                                 );
+                               }else if(snapshot.hasData){
+                                 if(snapshot.data!.size==0){
+                                   return Expanded(
+                                     child: GridView.count(
+                                       primary: false,
+                                       padding: const EdgeInsets.only(
+                                           top: 30, left: 80, right: 80, bottom: 10),
+                                       crossAxisSpacing: 0,
+                                       mainAxisSpacing: 20,
+                                       crossAxisCount: 1,
+                                       children: <Widget>[
+                                         Container(
+                                           padding: const EdgeInsets.only(
+                                               top: 0, left: 10, right: 10, bottom: 0),
+                                           child: (MaterialButton(
+                                             onPressed: () {},
+                                             height: 20,
+                                             minWidth: 20,
+                                             elevation: 10,
+                                             color: Colors.white,
+                                             shape: RoundedRectangleBorder(
+                                               borderRadius: BorderRadius.circular(10),
+                                             ),
+                                             child: Container(
+                                               child: Column(
+                                                   mainAxisAlignment: MainAxisAlignment.center,
+                                                   children: <Widget>[
+                                                     Image(
+                                                       image: AssetImage(
+                                                         'assets/icons/lock.png',
+                                                       ),
+                                                       height: 75,
+                                                     ),
+                                                     SizedBox(height: 20,),
+                                                     Text(
+                                                       'Engage Mission',
+                                                       textAlign: TextAlign.center,
+                                                       style: TextStyle(
+                                                         fontSize: 15,
+                                                         fontWeight: FontWeight.w700,
+                                                       ),
+                                                     ),
+                                                   ]),
+                                             ),
+                                           )),
+                                         ),
+                                         Container(
+                                           padding: const EdgeInsets.only(
+                                               top: 0, left: 10, right: 10, bottom: 0),
+                                           child: (MaterialButton(
+                                             onPressed: () {},
+                                             height: 10,
+                                             minWidth: 20,
+                                             elevation: 10,
+                                             color: Colors.white,
+                                             shape: RoundedRectangleBorder(
+                                               borderRadius: BorderRadius.circular(10),
+                                             ),
+                                             child: Container(
+                                               child: Column(
+                                                   mainAxisAlignment: MainAxisAlignment.center,
+                                                   children: <Widget>[
+                                                     Image(
+                                                       image: AssetImage(
+                                                         'assets/icons/unlock.png',
+                                                       ),
+                                                       height: 75,
+                                                       alignment: Alignment.center,
+                                                     ),
+                                                     SizedBox(height: 20,),
+                                                     Text(
+                                                       'Engage Mission',
+                                                       textAlign: TextAlign.center,
+                                                       style: TextStyle(
+                                                         fontSize: 15,
+                                                         fontWeight: FontWeight.w700,
+                                                       ),
+                                                     ),
+                                                   ]),
+                                             ),
+                                           )),
+                                         ),
+                                       ],
+                                     ),
+                                   );
+                                 }else{
+                                   return Column(
+                                     children: [
+                                       SizedBox(height: 70,),
+                                       Icon(Icons.not_interested_outlined, color: Colors.red, size: 50,),
+                                       SizedBox(height: 20,),
+                                       Container(child: Text("Ongoing training operation", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),) ,),
+                                     ],
+                                   );
+                                 }
+                               }else{
+                                 return Column(
+                                   children: [
+                                     SizedBox(height: 70,),
+                                     Icon(Icons.not_interested_outlined, color: Colors.red, size: 50,),
+                                     SizedBox(height: 20,),
+                                     Container(child: Text("Something went wrong", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),)),
+                                   ],
+                                 );
+                               }
+                              }
+                            );
+                          }else{
+                            return Column(
+                              children: [
+                                SizedBox(height: 70,),
+                                Icon(Icons.not_interested_outlined, color: Colors.red, size: 50,),
+                                SizedBox(height: 20,),
+                                Container(child: Text("Ongoing live operation", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),) ,),
+                              ],
+                            );
+                          }
+                        }else{
+                          return Column(
+                            children: [
+                              SizedBox(height: 70,),
+                              Icon(Icons.not_interested_outlined, color: Colors.red, size: 50,),
+                              SizedBox(height: 20,),
+                              Container(child: Text("Something went wrong", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),)),
+                            ],
+                          );
+                        }
                       }
-                    }else{
-                      return Container(child: Text("Something went wrong"));
-                    }
-                  }
+                    ),
+                  ],
                 ),
               ],
             ),
