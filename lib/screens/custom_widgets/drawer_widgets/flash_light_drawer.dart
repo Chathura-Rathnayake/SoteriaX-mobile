bdb996b2239082ae6823d41ab208de349de03804
nukezzzz <lawthenuka@gmail.com>
2021-09-21 13:12:57 +0530

    light on off code

--- lib/screens/custom_widgets/drawer_widgets/flash_light_drawer.dart
+++ lib/screens/custom_widgets/drawer_widgets/flash_light_drawer.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:soteriax/services/raspberryPi/light.dart';
 
 class FlashLightDrawer extends StatefulWidget {
   const FlashLightDrawer({Key? key}) : super(key: key);
@@ -10,6 +10,7 @@ class FlashLightDrawer extends StatefulWidget {
 }
 
 class _FlashLightDrawerState extends State<FlashLightDrawer> {
+  LightRPI light = LightRPI();
   @override
   Widget build(BuildContext context) {
     return Drawer(
@@ -22,46 +23,67 @@ class _FlashLightDrawerState extends State<FlashLightDrawer> {
                 height: 80,
                 padding: EdgeInsets.only(top: 40),
                 width: double.infinity,
-                child: Text("Flash Light", textAlign: TextAlign.center, style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 16), ),
+                child: Text(
+                  "Flash Light",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16),
+                ),
               ),
-              SizedBox(height: 20,),
+              SizedBox(
+                height: 20,
+              ),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 50.0),
-                child:Container(
-                ),
+                child: Container(),
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   IconButton(
-                      onPressed: (){
-
+                      onPressed: () {
+                        light.RPiLighton();
                       },
                       color: Colors.blue[900],
                       iconSize: 60,
-                      icon: Icon(Icons.flashlight_on, size: 60,)
+                      icon: Icon(
+                        Icons.flashlight_on,
+                        size: 60,
+                      )),
+                  SizedBox(
+                    width: 40,
                   ),
-                  SizedBox(width: 40,),
                   IconButton(
-                      onPressed: (){
-
+                      onPressed: () {
+                        light.RPiLightoff();
                       },
                       color: Colors.red[900],
                       iconSize: 60,
-                      icon: Icon(Icons.flashlight_off, size: 60,)
-                  ),
+                      icon: Icon(
+                        Icons.flashlight_off,
+                        size: 60,
+                      )),
                 ],
               ),
               Padding(
                 padding: const EdgeInsets.all(20.0),
                 child: Text(
                   "Toggle Flash-Light",
-                  textAlign: TextAlign.center, style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                 ),
               ),
               MaterialButton(
-                child: Text("BACK", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),),
-                onPressed: (){
+                child: Text(
+                  "BACK",
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+                onPressed: () {
                   Navigator.pop(context);
                 },
                 minWidth: 125,
---
+++ lib/services/raspberryPi/light.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+import 'package:http/http.dart' as http;
+
+class LightRPI {
+  Future<bool> RPiLighton() async {
+    print("works");
+    final response =
+        await http.get(Uri.parse('http://192.168.1.6:4000/lighton'));
+
+    if (response.statusCode == 200) {
+      print("Reached RPI endpoint");
+      // If the server did return a 200 OK response,
+      // then parse the JSON.
+      return Future.value(true);
+    } else {
+      print("Error cant reach RPI endpoint");
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      return Future.value(false);
+    }
+  }
+
+  Future<bool> RPiLightoff() async {
+    final response =
+        await http.get(Uri.parse('http://192.168.1.6:4000/lightoff'));
+
+    if (response.statusCode == 200) {
+      print("works 1");
+      // If the server did return a 200 OK response,
+      // then parse the JSON.
+      return Future.value(true);
+    } else {
+      print("works 2");
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      return Future.value(false);
+    }
+  }
+}
