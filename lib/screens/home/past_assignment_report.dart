2227b615e48982ddf1685fe90fb9e3c2589fc406
Iranga Mudalige <irangapriyam@gmail.com>
2021-09-22 00:47:44 +0530

    report

--- lib/screens/home/past_assignment_report.dart
+++ lib/screens/home/past_assignment_report.dart
@@ -3,7 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:soteriax/database/past_assignments_database_services.dart';
 import 'package:soteriax/screens/shared/display_timeline.dart';
-
+import 'package:syncfusion_flutter_charts/charts.dart';
 class PastAssignmentReport extends StatefulWidget {
   PastAssignmentReport({required this.assignmentId});
 
@@ -13,10 +13,15 @@ class PastAssignmentReport extends StatefulWidget {
   _PastAssignmentReportState createState() => _PastAssignmentReportState();
 }
 
+
 class _PastAssignmentReportState extends State<PastAssignmentReport> {
+
+  late List<GDPdata> _chartData;
+  late TooltipBehavior _tooltipBehaviour;
 
   @override
   void initState() {
+    _tooltipBehaviour=TooltipBehavior(enable: true);
     super.initState();
 
   }
@@ -47,60 +52,79 @@ class _PastAssignmentReportState extends State<PastAssignmentReport> {
                   return Container(child: Text("No data retrieved"),);
                 }else{
                   var assignment=snapshot.data!;
-                  return Container(
-                    padding: EdgeInsets.symmetric(vertical: 40, horizontal: 25),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          children: [
-                            Text("Assignment Id: ${assignment.id}",
-                              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                              overflow: TextOverflow.clip,
-                            ),
+                  _chartData = getChartData(assignment);
+                  _tooltipBehaviour=TooltipBehavior(enable: true);
+                  return SingleChildScrollView(
+                    child: Container(
+                      padding: EdgeInsets.symmetric(vertical: 20, horizontal: 25),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SfCircularChart(
+                            title: ChartTitle(text: "Timing in training operation steps"),
+                            legend: Legend(isVisible: true, overflowMode: LegendItemOverflowMode.wrap),
+                            tooltipBehavior: _tooltipBehaviour,
+                            series: <CircularSeries>[
+                              PieSeries<GDPdata,String>(
+                                dataSource: _chartData,
+                                xValueMapper: (GDPdata data,_) =>data.continent,
+                                yValueMapper: (GDPdata data,_) => data.gdp,
+                                //dataLabelSettings: DataLabelSettings(isVisible:true),
+                                enableTooltip: true,
+                              )
+                            ],
 
-                          ],
-                        ),
-                        SizedBox(height: 20,),
-                        Text("Title: ${assignment.get('title')}",
-                          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                          overflow: TextOverflow.ellipsis,
-                        ),SizedBox(height: 20,),
-                        Text("Summary: ${assignment.get('summary')}",
-                          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                        SizedBox(height: 20,),
-                        Text("Drone Pilot: ${assignment.get('participants')['dronePilotName']}",
-                          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                        SizedBox(height: 20,),
-                        Text("Package Handler: ${assignment.get('participants')['mobileHandelerName']}",
-                          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                        SizedBox(height: 20,),
-                        Text("Swimmer: ${assignment.get('participants')['swimmerName']}",
-                          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                        SizedBox(height: 20,),
-                        Text("Sea condition: ${assignment.get('seaCondition')}",
-                          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                        SizedBox(height: 20,),
-                        Text("Date: ${assignment.get('date')}",
-                          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                        SizedBox(height: 20,),
-                        Text("Start time: ${assignment.get('startTime')}",
-                          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                      ],
+                          ),
+                          Row(
+                            children: [
+                              Text("Assignment Id: ${assignment.id}",
+                                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                                overflow: TextOverflow.clip,
+                              ),
+
+                            ],
+                          ),
+                          SizedBox(height: 20,),
+                          Text("Title: ${assignment.get('title')}",
+                            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                            overflow: TextOverflow.ellipsis,
+                          ),SizedBox(height: 20,),
+                          Text("Summary: ${assignment.get('summary')}",
+                            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          SizedBox(height: 20,),
+                          Text("Drone Pilot: ${assignment.get('participants')['dronePilotName']}",
+                            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          SizedBox(height: 20,),
+                          Text("Package Handler: ${assignment.get('participants')['mobileHandelerName']}",
+                            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          SizedBox(height: 20,),
+                          Text("Swimmer: ${assignment.get('participants')['swimmerName']}",
+                            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          SizedBox(height: 20,),
+                          Text("Sea condition: ${assignment.get('seaCondition')}",
+                            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          SizedBox(height: 20,),
+                          Text("Date: ${assignment.get('date')}",
+                            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          SizedBox(height: 20,),
+                          Text("Start time: ${assignment.get('startTime')}",
+                            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ],
+                      ),
                     ),
                   );
                 }
@@ -112,6 +136,42 @@ class _PastAssignmentReportState extends State<PastAssignmentReport> {
       ),
     );
   }
+  List<GDPdata> getChartData(DocumentSnapshot assignment){
+    int first=0;
+    if(assignment.get('trainingTimes')[1]!=""){
+      first=Timestamp.fromMillisecondsSinceEpoch(assignment.get('trainingTimes')[1]).seconds;
+    }
+    int second=0;
+    if(assignment.get('trainingTimes')[2]!=""){
+      second=Timestamp.fromMillisecondsSinceEpoch(assignment.get('trainingTimes')[2]).seconds;
+    }
+    int third=0;
+    if(assignment.get('trainingTimes')[3]!=""){
+      third=Timestamp.fromMillisecondsSinceEpoch(assignment.get('trainingTimes')[3]).seconds;
+    }
+    int fourth=0;
+    if(assignment.get('trainingTimes')[4]!=""){
+      fourth=Timestamp.fromMillisecondsSinceEpoch(assignment.get('trainingTimes')[4]).seconds;
+    }
+    // String  first=assignment.get('trainingTimes')[1];
+    // String  second=assignment.get('trainingTimes')[2];
+    // String  third=assignment.get('trainingTimes')[3];
+    // String  fourth=assignment.get('trainingTimes')[4];
+    final List<GDPdata> chartData = [
+      //GDPdata("Mission initiated", assignment.get('trainingTimes')[0]),
+      GDPdata("UAV reached",first),
+      GDPdata("Restube droped",second-first),
+      GDPdata("Lifeguard reached",third-second),
+      GDPdata("Rescue completed",fourth-third),
+    ];
+    return chartData;
+  }
+}
+
+class GDPdata{
+  GDPdata(this.continent,this.gdp);
+  final String continent;
+  final int gdp;
 }
 
 
